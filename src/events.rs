fd606cecb9b49a3f16d4084f70b058788ddb70c1
ninelie-daybreak <1822903064@qq.com>
2022-07-25 22:01:29 +0800

    add new macro 'struct_events'

--- src/events.rs
+++ src/events.rs
@@ -1,45 +1,42 @@
-use sdl2::EventPump;
+macro_rules! struct_events {
+    () => {
+        use sdl2::EventPump;
 
-pub struct Events {
-    pump: EventPump,
 
-    pub quit: bool,
-    pub key_escape: bool,
-}
+        pub struct ImmediateEvents;
 
-impl Events {
-    pub fn new(pump: EventPump) -> Events {
-        Events {
-            pump: pump,
-
-            quit: false,
-            key_escape: false,
-        }
-    }
-
-    /// Update the events record
-    pub fn pump(&mut self) {
-        // If the SDL context is dropped, then poll_iter() will simply stop
-        // yielding any input
-        for event in self.pump.poll_iter() {
-            use sdl2::event::Event::*;
-            use sdl2::keyboard::Keycode::*;
-
-            match event {
-                Quit { .. } => self.quit = true,
-                
-                KeyDown { keycode, .. } => match keycode {
-                    Some(Escape) => self.key_escape = true,
-                    _ => {}
-                },
-
-                KeyUp { keycode, ..} => match keycode {
-                    Some(Escape) => self.key_escape = false,
-                    _ => {}
-                },
-
-                _ => {}
+        impl ImmediateEvents {
+            pub fn new() -> ImmediateEvents {
+                ImmediateEvents
             }
         }
-    }
+
+        pub struct Events {
+            pump: EventPump,
+            pub now: ImmediateEvents,
+        }
+
+        impl Events {
+            pub fn new(pump: EventPump) -> Events {
+                Events {
+                    pump: pump,
+                    now : ImmediateEvents::new()
+                }
+            }
+
+            pub fn pump(&mut self) {
+                self.now = ImmediateEvents::new();
+
+                for event in self.pump.poll_iter() {
+                    use sdl2::event::Event::*;
+                    use sdl2::keyboard::Keycode::*;
+
+                    match event {
+                        _ => {}
+                    }
+                }
+            }
+        }
+    };
 }
+
