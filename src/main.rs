ad40bb6e861ff892ae9173532a948e6e0222e528
ninelie-daybreak <1822903064@qq.com>
2022-08-13 11:18:52 +0800

    change the loop logic & add the frame count

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ extern crate sdl2;
 mod phi;
 mod views;
 
-use sdl2::pixels::Color;
-use phi::Events;
+use phi::{Events, Phi, View, ViewAction};
 
 
 fn main() {
     // Initialize sdl2
     let sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
 
     // Create the window
     let window = video.window("ArcadeRS Shooter", 800, 600)
@@ -18,22 +18,55 @@ fn main() {
         .opengl()
         .build()
         .unwrap();
-    
-    let mut canvas = window.into_canvas().accelerated().build().unwrap();
 
-    // Prepare the events record
-    let mut events = Events::new(sdl_context.event_pump().unwrap());
+    // Create the context
+    let mut context = Phi {
+        events: Events::new(sdl_context.event_pump().unwrap()),
+        renderer: window.into_canvas()
+            .accelerated()
+            .build().unwrap(),
+    };
+    
+    // Create the default view
+    let mut current_view: Box<dyn View> = Box::new(crate::views::DefaultView);
+
+    // Frame timing
+    let interval = 1_000 / 60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
 
     loop {
-        events.pump();
+        // Frame timing (bis)
 
-        if events.now.quit || events.now.key_escape == Some(true) {
-            break;
+        let now = timer.ticks();
+        let dt = now - before;
+        let elapsed = dt as f64 / 1_000.0;
+
+        // If the time elapsed since the last frame is too small, wait out the
+        // difference and try again.
+        if dt < interval {
+            timer.delay(interval - dt);
+            continue;
         }
 
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
-        canvas.present();
+        before = now;
+        fps += 1;
 
+        if now - last_second > 1_000 {
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps = 0;
+        }
+
+
+        // Logic & rendering
+
+        context.events.pump();
+
+        match current_view.render(&mut context, 0.01) {
+            ViewAction::None => context.renderer.present(),
+            ViewAction::Quit => break,
+        }
     }
 }
