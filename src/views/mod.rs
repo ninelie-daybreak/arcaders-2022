b7d70800b09db65224c44878afc880b6a860eea0
ninelie-daybreak <1822903064@qq.com>
2022-08-29 16:16:20 +0800

    render a new iamge here

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,11 +1,15 @@
 use crate::phi::{Phi, View, ViewAction};
 use sdl2::pixels::Color;
 use crate::phi::data::Rectangle;
+use std::path::Path;
+use sdl2::render::{Texture, TextureQuery};
+use sdl2::image::LoadTexture;
 
 const PLAYER_SPEED:f64 = 180.0;
 
 struct Ship {
-    rect: Rectangle
+    rect: Rectangle,
+    tex: Texture,
 }
 pub struct ShipView {
     player: Ship,
@@ -13,14 +17,23 @@ pub struct ShipView {
 
 impl ShipView {
     pub fn new(phi: &mut Phi) -> ShipView {
+        //? Load the texture from the filesystem
+        //? If it cannot be found, then there is no point in continuing: panic!
+        let tex = phi.renderer.texture_creator().load_texture(Path::new("src/assets/spaceship.png")).unwrap();
+
+        //? Destructure some properties of the texture, notably width and
+        //? height, which we will use for the ship's bounding box.
+        let TextureQuery { width, height, ..} = tex.query();
+
         ShipView {
             player: Ship { 
                 rect: Rectangle {
                     x: 64.0,
                     y: 64.0,
-                    w: 32.0,
-                    h: 32.0,
-                } 
+                    w: width as f64,
+                    h: height as f64,
+                },
+                tex: tex,
             }
         }
     }
@@ -79,6 +92,23 @@ impl View for ShipView {
         // Render the scene
         phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
         phi.renderer.fill_rect(self.player.rect.to_sdl()).unwrap();
+
+        //? We add this part:
+        // Render the ship
+        //? The texture to render is `self.player.text`(we borrow it mutably)
+        phi.renderer.copy(&mut self.player.tex,
+            //? The "source region" of the image. Here, we take the entire image, from
+            //? the top-left corner (0,0) to the bottom-right one (rect.w, rect.h).
+            Rectangle {
+                x: 0.0,
+                y: 0.0,
+                w: self.player.rect.w,
+                h: self.player.rect.h,
+            }.to_sdl(),
+            //? The destination of the image. We simply provide the bounding box, the
+            //? renderer takes care fo the rest.
+            self.player.rect.to_sdl()
+        ).unwrap();
 
         ViewAction::None
     }
