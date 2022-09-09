7ce4c268478276267882130040c2056e1f5ff56e
ninelie-daybreak <1822903064@qq.com>
2022-09-09 20:17:13 +0800

    add structure 'Sprite' to wrap texture

---
+++ src/phi/gfx.rs
@@ -0,0 +1,64 @@
+use crate::phi::data::Rectangle;
+use std::cell::RefCell;
+use std::path::Path;
+use std::rc::Rc;
+use sdl2::render::{WindowCanvas, Texture};
+use sdl2::image::LoadTexture;
+
+#[derive(Clone)]
+pub struct Sprite {
+    tex: Rc<RefCell<Texture>>,
+    src: Rectangle,
+}
+
+impl Sprite {
+    pub fn new(texture: Texture) -> Sprite {
+        let tex_query = texture.query();
+
+        Sprite {
+            tex: Rc::new(RefCell::new(texture)),
+            src: Rectangle {
+                w: tex_query.width as f64, 
+                h: tex_query.height as f64,
+                x: 0.0,
+                y: 0.0,
+            }
+        }
+    }
+
+    /// Creates a new sprite form an image file located at the given path,
+    /// Returns `Some` if the file could be read, and `None` otherwise.
+    pub fn load(renderer: &WindowCanvas, path: &str) -> Option<Sprite> {
+        renderer.texture_creator().load_texture(Path::new(path)).ok().map(Sprite::new)
+    }
+
+    /// Returns a new `Sprite` representing a sub-region of the current one.
+    /// The provided `rect` is relative to the currently held region.
+    /// Returns `Some` if the `rect` is valid, i.e. included in the current
+    /// region, and `None` otherwise.
+    pub fn region(&self, rect: Rectangle) -> Option<Sprite> {
+        let new_src = Rectangle {
+            x: rect.x + self.src.x,
+            y: rect.x + self.src.y,
+            ..rect
+        };
+
+        // Verify that the region requested region is inside of the current one
+        if self.src.contains(new_src) {
+            Some(Sprite {
+                tex: self.tex.clone(),
+                src: new_src,
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn size(&self) -> (f64, f64) {
+        (self.src.w, self.src.h)
+    }
+
+    pub fn render(&self, renderer: &mut WindowCanvas, dest: Rectangle) {
+        renderer.copy(&mut self.tex.borrow_mut(), self.src.to_sdl(), dest.to_sdl());
+    }
+}
