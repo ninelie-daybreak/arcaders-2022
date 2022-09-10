a6d0d7effc9e5b30b464c5bb661fa94342b72ede
ninelie-daybreak <1822903064@qq.com>
2022-09-10 15:55:35 +0800

    add background to the game

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -2,12 +2,15 @@ use crate::phi::{Phi, View, ViewAction};
 use crate::phi::data::Rectangle;
 use crate::phi::gfx::{Sprite, CopySprite};
 use sdl2::pixels::Color;
+use sdl2::render::WindowCanvas;
 
 /// Pixels traveled by the player's ship every second, when it is moving
 const PLAYER_SPEED:f64 = 180.0;
 
 const SHIP_W: f64 = 43.0;
 const SHIP_H: f64 = 39.0;
+
+const DEBUG: bool = false;
 
 /// The different states our ship might be in. In the image, they're ordered
 /// from left to right, then top to bottom.
@@ -29,8 +32,21 @@ struct Ship {
     sprites: Vec<Sprite>,
     current: ShipFrame,
 }
+
+#[derive(Clone)]
+struct Background {
+    pos: f64,
+    // The amount of pixels moved to the left every second
+    vel: f64,
+    sprite: Sprite,
+}
+
 pub struct ShipView {
     player: Ship,
+
+    bg_back: Background,
+    bg_middle: Background,
+    bg_front: Background,
 }
 
 impl ShipView {
@@ -62,6 +78,24 @@ impl ShipView {
                 },
                 sprites: sprites,
                 current: ShipFrame::MidNorm,
+            },
+
+            bg_back: Background {
+                pos: 0.0,
+                vel: 20.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starBG.png").unwrap(),
+            },
+
+            bg_middle: Background {
+                pos: 0.0,
+                vel: 40.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starMG.png").unwrap(),
+            },
+
+            bg_front: Background {
+                pos: 0.0,
+                vel: 80.0,
+                sprite: Sprite::load(&mut phi.renderer, "assets/starFG.png").unwrap(),
             }
         }
     }
@@ -126,19 +160,60 @@ impl View for ShipView {
             else if dx < 0.0 && dy > 0.0   { ShipFrame::DownSlow }
             else { unreachable!() };
 
-        // Clear the screen
+        // Render the scene
         phi.renderer.set_draw_color(Color::RGB(0, 0, 0));
         phi.renderer.clear();
 
-        // Render the scene
-        phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
-        phi.renderer.fill_rect(self.player.rect.to_sdl()).unwrap();
+        // Render the Backgrounds
+        self.bg_back.render(&mut phi.renderer, elapsed);
+        self.bg_middle.render(&mut phi.renderer, elapsed);
 
+        // Render the bounding box(for debugging purposes)
+        if DEBUG {
+            phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
+            phi.renderer.fill_rect(self.player.rect.to_sdl());
+        }
+
+        // Render the ship
         phi.renderer.copy_sprite(
             &self.player.sprites[self.player.current as usize],
             self.player.rect
         );
 
+        // Render the foreground
+        self.bg_front.render(&mut phi.renderer, elapsed);
+
         ViewAction::None
+    }
+}
+
+impl Background {
+    fn render(&mut self, renderer: &mut WindowCanvas, elapsed: f64) {
+        // We define a logical position as depending solely on the time and the
+        // dimensions of the image, not on the screen's size.
+        let size = self.sprite.size();
+        self.pos += self.vel * elapsed;
+        if self.pos > size.0 {
+            self.pos -= size.0;
+        }
+
+        // We determine the scale ratio of the window to the sprite.
+        let (win_w, win_h) = renderer.output_size().unwrap();
+        let scale = win_h as f64 / size.1;
+
+        // We render as many copies of the background as necessary to fill the screen
+        let mut physical_left = -self.pos * scale;
+
+        while physical_left < win_w as f64 {
+            //? While the left of the image is still inside of the window...
+            renderer.copy_sprite(&self.sprite, Rectangle {
+                x: physical_left,
+                y: 0.0,
+                w: size.0 * scale,
+                h: win_h as f64,
+            });
+
+            physical_left += size.0 * scale;
+        }
     }
 }
