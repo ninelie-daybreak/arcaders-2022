d6797e8cb0b88505a69e05d258045e80a4e6dfdf
ninelie-daybreak <1822903064@qq.com>
2022-09-11 14:14:28 +0800

    initial view has been changed to MainMenu

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ mod views;
 
 fn main() {
     crate::phi::spawn("ArcadeRS Shooter", |phi| {
-        Box::new(crate::views::ShipView::new(phi))
+        Box::new(crate::views::main_menu::MainMenuView::new(phi))
     });
 }
